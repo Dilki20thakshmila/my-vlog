70714334dd293229a601f852faf63cb9ca9a12b7
DilkiThakshmila <dilkithakshmila@gmail.com>
2024-02-19 22:37:25 +0530

    creating frontend for vlog web site

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js or index.js
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Homepage from '../pages/Homepage';
+// import VlogDetailPage from './pages/VlogDetailPage';
+// import UserProfilePage from './pages/UserProfilePage';
+// import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/" component={Homepage} />
+                {/* <Route path="/vlog/:id" component={VlogDetailPage} />
+                <Route path="/user/:userId" component={UserProfilePage} />
+                <Route component={NotFoundPage} /> */}
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,15 @@
+// Homepage.js
+
+import React from 'react';
+
+const Homepage = () => {
+    return (
+        <div>
+            <h1>Welcome to Our Vlog Website!</h1>
+            <p>Discover and explore amazing vlogs from talented creators.</p>
+            {/* Add more JSX elements for displaying featured vlogs, trending vlogs, etc. */}
+        </div>
+    );
+}
+
+export default Homepage;
