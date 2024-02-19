67a67605c12b8fb7817f8456320eee9ba51c1572
DilkiThakshmila <dilkithakshmila@gmail.com>
2024-02-19 23:01:07 +0530

    add background image

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,11 @@
 // Homepage.js
 
 import React from 'react';
+import './App.css';
 
 const Homepage = () => {
     return (
-        <div>
+        <div className="homepage-background">
             <h1>Welcome to Our Vlog Website!</h1>
             <p>Discover and explore amazing vlogs from talented creators.</p>
             {/* Add more JSX elements for displaying featured vlogs, trending vlogs, etc. */}
